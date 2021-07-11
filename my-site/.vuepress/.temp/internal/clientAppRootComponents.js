986e096f6aafde1c9f3a33ccb76b05b92ba90de8
Zhang <761042182@qq.com>
2021-07-11 21:16:35 +0800

    v0.12
    因为yuque-plugin-vuepress已经不再维护，采用yuque+hexo的方案建站

--- my-site/.vuepress/.temp/internal/clientAppRootComponents.js
+++
@@ -1,5 +0,0 @@
-import clientAppRootComponent0 from 'C:/Users/76104/Desktop/hduer/my-site/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
-
-export const clientAppRootComponents = [
-  clientAppRootComponent0,
-]
--- my-site/.vuepress/.temp/internal/pagesData.js
+++
@@ -1,4 +0,0 @@
-export const pagesData = {
-  // path: /404.html
-  "v-3706649a": () => import(/* webpackChunkName: "v-3706649a" */"C:/Users/76104/Desktop/hduer/my-site/.vuepress/.temp/pages/404.html.js").then(({ data }) => data),
-}
